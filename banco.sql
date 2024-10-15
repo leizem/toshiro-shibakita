7c4ad1d24b1cd43ab627f9bcfea9c97102c3734e
Tiago Monteiro <leizem@gmail.com>
2024-10-15 16:36:18 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -2,7 +2,7 @@ CREATE TABLE dados (
     AlunoID int,
     Nome varchar(50),
     Sobrenome varchar(50),
-    Celular 
+    Celular varchar(11),
     Endereco varchar(150),
     Cidade varchar(50),
     Host varchar(50)
